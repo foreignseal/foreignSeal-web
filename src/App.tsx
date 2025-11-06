0b33d6d3177fde37613e72cb85e24d98d5b45223
foreignseal <goodnight.isma@gmail.com>
2025-11-06 14:09:26 +0000

    Initial Commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+        <h1>the seal hasn't taken the plane</h1>
+        <p>We are awaiting their arrival...</p>
+    </>
+  )
+}
+
+export default App
